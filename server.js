dcc4d599388f5a0b2388a711eff18f8a4602076a
Arnav Gudibande <arnav.gudibande@gmail.com>
2016-02-28 11:23:38 -0800

    Initialize repo

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 var express = require("express");
 var app = express();
+var PORT = 3000;
 
-app.get("/", function (req, res){
-  res.send("Hello Express!");
-});
+var middleware = {
+  requireAuthentication: function (req, res, next) {
+    console.log("private route hit");
+    next();
+  },
+  logger: function (req, res, next) {
+    console.log("Request @ " + new Date().toString() + ": " + req.method + " " + req.originalUrl);
+    next();
+  }
+};
 
-app.get("/about", function (req, res){
+app.use(middleware.logger);
+
+app.get("/about", middleware.requireAuthentication, function (req, res){
   res.send("About Us")
 });
 
-app.listen(3000);
+app.use(express.static(__dirname+"/public"));
+
+app.listen(PORT, function () {
+  console.log("Express Server Started on port "+PORT+"!");
+});

1bb3f900ac034ff49f4752b3d7bea233f2f0a11e
Arnav Gudibande <arnav.gudibande@gmail.com>
2016-03-09 08:06:51 -0800

    update server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 3000;
-var middleware = require("./middleware.js");
+var express = require("express");//setting up the express module
+var app = express();//creating an app that uses express
+var PORT = process.env.PORT || 3000;//setting up the output port
+var middleware = require("./middleware.js");//accessing the middleware stored in a separate file
 
 
-app.use(middleware.logger);
+app.use(middleware.logger);//setting up the app to use the middleware logger
 
-app.get("/about", middleware.requireAuthentication, function (req, res){
+app.get("/about", middleware.requireAuthentication, function (req, res){ //if the server is set up to the /about page, it will call the requireAuthentication function
   res.send("About Us!")
 });
 
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname+"/public"));//it uses the express static
 
-app.listen(PORT, function () {
+app.listen(PORT, function () {//setting up the app to listen on port 3000
   console.log("Express Server Started on port "+PORT+"!");
 });
